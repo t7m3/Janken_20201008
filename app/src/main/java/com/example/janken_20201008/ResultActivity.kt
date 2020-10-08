6adc66f3d128010c76cd96da4c33c44b7233187f
t7m3 <toda7.mk3@gmail.com>
2020-10-08 12:35:00 +0900

    2020-10-08(木) 2回目のコミット とりあえず、元の画面に戻ることだけを追加した。

--- app/src/main/java/com/example/janken_20201008/ResultActivity.kt
+++ app/src/main/java/com/example/janken_20201008/ResultActivity.kt
@@ -2,10 +2,13 @@ package com.example.janken_20201008
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        backButton.setOnClickListener { finish() }
     }
 }
