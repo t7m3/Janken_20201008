78ae913adae1c96a8003ae96129260eb3486504b
t7m3 <toda7.mk3@gmail.com>
2020-10-08 12:31:53 +0900

    2020-10-08(木) 1回目のコミット ボタンをタップすると画面遷移する。まだ元の画面には戻れない。

--- app/src/main/java/com/example/janken_20201008/MainActivity.kt
+++ app/src/main/java/com/example/janken_20201008/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.janken_20201008
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View){
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
 }
